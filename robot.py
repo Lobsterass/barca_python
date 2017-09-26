20b43bd84d40dc0f298a396aec8a4efaecdfc8fa
Lobsterass <Lobsterass@users.noreply.github.com>
2017-09-26 15:05:33 +0200

    med for-loop

--- robot.py
+++ robot.py
@@ -12,7 +12,10 @@ class Product:
 		return self.price * self.count * self.tax
 
 products = [Product(price=900, count=2, tax=1.25), Product(price=100, count=1, tax=1.06)]
-total_price = products[0].price_with_tax() + products[1].price_with_tax()
 
-print(total_price) 
+total_price = 0
+for product in products:
+	total_price = total_price + product.price_with_tax()
+print(total_price)
+
 
