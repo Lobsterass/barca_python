5c9094ce4f2915c13fbbe51d1731166e4496f969
Lobsterass <Lobsterass@users.noreply.github.com>
2017-09-26 15:30:49 +0200

    lagt till namn på produkter

--- robot.py
+++ robot.py
@@ -1,25 +1,28 @@
 class Product:
+	name = ""
 	price = 0
 	count = 0
 	tax = 1
 
-	def __init__(self, price, count, tax):
+	def __init__(self, name, price, count, tax):
+		self.name = name
 		self.price = price
 		self.count = count
 		self.tax = tax
 
 	def price_with_tax(self):
-		total = self.price * self.count *self.tax
+		total = self.price * self.count * self.tax
 		if total > 500:
 			return 0.9 * total
 		else:
 			return total
 
-products = [Product(price=900, count=2, tax=1.25), Product(price=100, count=1, tax=1.06), Product(price=200, count=2, tax=1.25)]
+products = [Product(name="äpplen", price=900, count=2, tax=1.25), Product(name="apelsin", price=100, count=1, tax=1.06), Product(name="päron", price=200, count=2, tax=1.25)]
 
 total_price = 0
 for product in products:
 	total_price = total_price + product.price_with_tax()
-print(total_price)
 
+for product in products:
+	print(product.name, product.price_with_tax())
 
